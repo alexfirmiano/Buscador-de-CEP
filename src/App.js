8b264fdb60707349256f2c652656def66c77113e
AlexBF_Pro <alexfirmiano2@gmail.com>
2022-09-14 18:02:23 -0300

    Modelando as aplicações

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { FiSearch } from 'react-icons/fi'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+        type="text" 
+        placeholder="Digite seu CEP aqui..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color='#FFF' />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 25935-087</h2>
+
+        <span>Rua Testando informações</span>
+        <span>Complemento: info qualquer</span>
+        <span>Centro</span>
+        <span>Rio de Janeiro - RJ</span>
+      </main>
 
     </div>
   );
